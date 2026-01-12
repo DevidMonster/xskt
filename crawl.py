fe8e858981b434cf7ee942dba4f1ccbdee98bad8
QuangDang <dangnq@phx-smartschool.com>
2026-01-12 10:23:25 +0700

    update code

--- crawl.py
+++ crawl.py
@@ -8,18 +8,23 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timedelta
 import json
 import time
+import pytz
 
 options = Options()
 options.add_argument("--headless=new")
 options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
-wait = WebDriverWait(driver, 30)
+wait = WebDriverWait(driver, 40)
 
 BASE_URL = "https://ketqua04.net/so-ket-qua"
 results = []
-today = datetime.now()
+
+vn_tz = pytz.timezone("Asia/Ho_Chi_Minh")
+today = datetime.now(vn_tz)
 
 for i in range(300):
     day = today - timedelta(days=i)
@@ -34,6 +39,7 @@ for i in range(300):
     driver.execute_script("kqv1.skq_quick_submit(300);")
 
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "phoi-size")))
+    time.sleep(1)
 
     script = """
     let arr = [];
@@ -51,7 +57,7 @@ for i in range(300):
     })
 
     print("✔", date_str, ":", len(data), "số")
-    time.sleep(0.7)
+    time.sleep(0.6)
 
 driver.quit()
 
